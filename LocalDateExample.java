8c05a0817f0ab0ceaf85c6e87d42615b2edee294
Sri Skandarajah <asriskan@gmail.com>
2018-09-09 11:47:49 -0400

    minor updates to do further testing

--- LocalDateExample.java
+++ LocalDateExample.java
@@ -19,8 +19,8 @@ public class LocalDateExample {
 		System.out.println("Current Date="+today);
 		
 		//Creating LocalDate by providing input arguments
-		LocalDate firstDay_2015 = LocalDate.of(2015, Month.JANUARY, 1);
-		System.out.println("Specific Date="+firstDay_2015);
+		LocalDate firstDay_2015 = LocalDate.of(2016, Month.JANUARY, 1); // change in Master Branch
+		System.out.println("Specific Date="+firstDay_2016);
 		
 		
 		//Try creating date by providing invalid inputs
