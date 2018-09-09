210b5c83a3b70043d7d98bce472bcd278f493908
Sri Skandarajah <asriskan@gmail.com>
2018-09-09 10:16:24 -0400

    minor updates as part of testing

--- LocalDateExample.java
+++ LocalDateExample.java
@@ -19,8 +19,8 @@ public class LocalDateExample {
 		System.out.println("Current Date="+today);
 		
 		//Creating LocalDate by providing input arguments
-		LocalDate firstDay_2014 = LocalDate.of(2014, Month.JANUARY, 1);
-		System.out.println("Specific Date="+firstDay_2014);
+		LocalDate firstDay_2015 = LocalDate.of(2015, Month.JANUARY, 1);
+		System.out.println("Specific Date="+firstDay_2015);
 		
 		
 		//Try creating date by providing invalid inputs
@@ -39,8 +39,8 @@ public class LocalDateExample {
 		LocalDate dateFromBase = LocalDate.ofEpochDay(365);
 		System.out.println("365th day from base date= "+dateFromBase);
 		
-		LocalDate hundredDay2014 = LocalDate.ofYearDay(2014, 100);
-		System.out.println("100th day of 2014="+hundredDay2014);
+		LocalDate twoHundredDay2018 = LocalDate.ofYearDay(2018, 200);
+		System.out.println("200th day of 2014="+hundredDay2018);
 	}
 
 }
